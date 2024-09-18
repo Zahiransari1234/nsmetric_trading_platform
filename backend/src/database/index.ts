9120b925acff2af032fd27ebf41bd8f9b0903b51
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-09-18 13:19:30 +0530

    revamp to javascript login and registration features added with change password, todo: add wallet details

--- backend/src/database/index.ts
+++
@@ -1,8 +0,0 @@
-export const connectDB = async () => {
-  try {
-    //await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`) //database connection using mongoose
-    console.log("Database Connected");
-  } catch (error) {
-    console.log(`My Database Error ${error}`);
-  }
-};
